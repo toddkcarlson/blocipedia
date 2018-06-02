0e6748c9f955847abe6f8e6765252317ec8ad48a
Todd Carlson <tcarlson@carlsonsw.com>
2018-06-02 15:05:47 -0400

    confirmable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,5 +52,15 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address:              'smtp.gmail.com',
+     port:                 587,
+     domain:               'gmail.com',
+     user_name:            'toddkcarlson@gmail.com',
+     password:             'my_password',
+     authentication:       'plain',
+     enable_starttls_auto: true  }
 end
