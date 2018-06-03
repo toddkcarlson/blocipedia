1ba0f778ea19e26b6dbec44f54183cb3b191791b
Todd Carlson <tcarlson@carlsonsw.com>
2018-06-03 09:52:29 -0400

    added default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+   enum role: [:standard, :premium, :admin]
+   after_initialize :set_default_role
+  
+   def set_default_role
+     self.role ||= :standard
+   end
+
 end
